07801d45041e4fd33feef05eab62a6036517fe01
Somchai2647 <somchai.b@cvc.ac.th>
2023-12-19 01:29:07 +0700

    Add sticker to reservation notification

--- src/classes/Reservation.ts
+++ src/classes/Reservation.ts
@@ -104,8 +104,8 @@ export default class Reservation {
 
       notify({
         message: `🍽️ การจองโต๊ะ (${payload.id}) \nโต๊ะที่: ${tableReservated.index} ถูกจองแล้ว \nโดย: ${reservation.name} \nเบอร์โทร: ${reservation.phone} \nอีเมล: ${reservation.email} \nรุ่น: ${reservation.generation} \nวิธีการชำระเงิน: ${reservation.method}`,
-        // stickerId: 51626507,
-        // stickerPackageId: 11538,
+        stickerId: 51626507,
+        stickerPackageId: 11538,
       }).then((res) =>
         console.log(`send notify: การจองโต๊ะ ${tableReservated.index}`)
       );
