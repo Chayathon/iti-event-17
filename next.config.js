b96147e681c3b9f9ae5daa8d8ed156a135bee96f
Somchai2647 <somchai.b@cvc.ac.th>
2023-12-25 00:24:09 +0700

    Update BASE_URL in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ let BASE_URL = "http://localhost:3000"
 
 //check env dev or prod
 if (process.env.NODE_ENV !== 'development') {
-  BASE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
+  BASE_URL = `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
 }
 
 const nextConfig = {
