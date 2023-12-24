07f42b9dee010e932cbac71d8c0ce260ef9cc0c1
Somchai2647 <somchai.b@cvc.ac.th>
2023-12-24 18:51:11 +0700

    Remove "Add to wishlist" button from ProductCard component

--- src/pages/shop/[...product.tsx].tsx
+++ src/pages/shop/[...product.tsx].tsx
@@ -164,11 +164,6 @@ export const ProductCard = ({ data }: { data?: ProductData }) => {
                     Add to Cart
                   </button>
                 </div>
-                <div className="w-full px-4 mb-4 lg:mb-0 lg:w-1/2">
-                  <button className="flex items-center justify-center w-full p-4 text-blue-500 border border-blue-500 rounded-md dark:text-gray-200 dark:border-blue-600 hover:bg-blue-600 hover:border-blue-600 hover:text-gray-100 dark:bg-blue-600 dark:hover:bg-blue-700 dark:hover:border-blue-700 dark:hover:text-gray-300">
-                    Add to wishlist
-                  </button>
-                </div>
               </div>
             </div>
           </div>
