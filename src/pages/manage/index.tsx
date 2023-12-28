352ee707028eaba45fbaba2910f9c1cb1ee0ead3
Somchai2647 <somchai.b@cvc.ac.th>
2023-12-29 01:23:50 +0700

    Refactor code to use async/await for fetching stat data

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -3,23 +3,35 @@ import AdminLayout from "@/components/layouts/AdminLayout";
 import Stat from "@/components/Cards/Manage/Stat";
 import { Data } from "@/interfaces/Stat.type";
 import ReservationTable from "@/components/Table/Manage/Reservation";
-import { fetcher } from "@/libs/axios";
+import axios, { fetcher } from "@/libs/axios";
 import useSWR from "swr";
 
 type Props = {};
 
 export default function Manage({}: Props) {
-  const { data: stat, error } = useSWR<Data>("/admin/reservation", fetcher);
+  // const { data: stat, error } = useSWR<Data>("/admin/reservation", fetcher);
 
-  if (error) {
-    console.error(error);
-    return <div>failed to load</div>;
+  // if (error) {
+  //   console.error(error);
+  //   return <div>failed to load</div>;
+  // }
+
+  async function getStat() {
+    try {
+      const res = await axios.get("/admin/reservation");
+      const x = await res.data;
+
+      return x;
+    } catch (error) {
+      console.error(error);
+    }
   }
-  
+
   return (
     <AdminLayout titile="ตรวจสอบรายการชำระเงิน">
+      <button className="btn-success" onClick={getStat}>dDD</button>
       {/* {JSON.stringify(stat)} */}
-      <Stat
+      {/* <Stat
         data={{
           success: stat?.SUCCESS.length,
           pending: stat?.PENDING.length,
@@ -29,7 +41,7 @@ export default function Manage({}: Props) {
       />
       <div className="my-2 ">
         <ReservationTable data={stat} />
-      </div>
+      </div> */}
     </AdminLayout>
   );
 }
