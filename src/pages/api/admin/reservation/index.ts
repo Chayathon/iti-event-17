110ac2157dca5bdd4886090c7a9fd41313abbf19
Somchai2647 <somchai.b@cvc.ac.th>
2023-12-29 12:02:10 +0700

    Refactor reservation API endpoint

--- src/pages/api/admin/reservation/index.ts
+++ src/pages/api/admin/reservation/index.ts
@@ -17,128 +17,20 @@ export default async function handler(
   try {
     switch (req.method) {
       case "GET":
-        const product = await ReservationProduct.getReservations();
-        const table =
-          (await ReservationTable.getReservations()) as ReservationTableData[];
+        const reservationTable = await ReservationTable.getReservations();
+        const reservationProduct = await ReservationProduct.getReservations();
 
-        let success = [];
-        let wait = [];
-        let pending = [];
-        let fails = [];
-
-        product.forEach((item: ReservationTableData) => {
-          item.type = "PRODUCT";
-          if (item.status === "COMPLETE") {
-            success.push(item);
-          } else if (item.status === "WAIT") {
-            wait.push(item);
-          } else if (item.status === "PENDING") {
-            pending.push(item);
-          } else if (item.status === "FAILS") {
-            fails.push(item);
-          }
-        });
-
-        table.forEach((item) => {
-          item.type = "TABLE";
-          if (item.status === "COMPLETE") {
-            success.push(item);
-          } else if (item.status === "WAIT") {
-            wait.push(item);
-          } else if (item.status === "PENDING") {
-            pending.push(item);
-          } else if (item.status === "FAILS") {
-            fails.push(item);
-          }
-        });
-
-        const data = {
-          SUCCESS: success,
-          WAIT: wait,
-          PENDING: pending,
-          FAILS: fails,
+        const payload = {
+          table: reservationTable,
+          product: reservationProduct,
         };
 
-        res.status(200).json({ message: "success", data: data });
+        res.status(200).json({ message: "success", data: payload });
         break;
 
       default:
         break;
     }
-
-    res.status(200).json({ message: "success", data: [] });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-}
-
-const processReservations = async (
-  reservations: ReservationTableData[],
-  type: string,
-  success: ReservationTableData[],
-  wait: ReservationTableData[],
-  pending: ReservationTableData[],
-  fails: ReservationTableData[]
-) => {
-  reservations.forEach((item: ReservationTableData) => {
-    item.type = type;
-    if (item.status === "COMPLETE") {
-      success.push(item);
-    } else if (item.status === "WAIT") {
-      wait.push(item);
-    } else if (item.status === "PENDING") {
-      pending.push(item);
-    } else if (item.status === "FAILS") {
-      fails.push(item);
-    }
-  });
-};
-
-async function getReservation(req: NextApiRequest, res: NextApiResponse<Data>) {
-  try {
-    const product = await ReservationProduct.getReservations();
-    const table =
-      (await ReservationTable.getReservations()) as ReservationTableData[];
-
-    let success = [];
-    let wait = [];
-    let pending = [];
-    let fails = [];
-
-    product.forEach((item: ReservationTableData) => {
-      item.type = "PRODUCT";
-      if (item.status === "COMPLETE") {
-        success.push(item);
-      } else if (item.status === "WAIT") {
-        wait.push(item);
-      } else if (item.status === "PENDING") {
-        pending.push(item);
-      } else if (item.status === "FAILS") {
-        fails.push(item);
-      }
-    });
-
-    table.forEach((item) => {
-      item.type = "TABLE";
-      if (item.status === "COMPLETE") {
-        success.push(item);
-      } else if (item.status === "WAIT") {
-        wait.push(item);
-      } else if (item.status === "PENDING") {
-        pending.push(item);
-      } else if (item.status === "FAILS") {
-        fails.push(item);
-      }
-    });
-
-    const data = {
-      SUCCESS: success,
-      WAIT: wait,
-      PENDING: pending,
-      FAILS: fails,
-    };
-
-    res.status(200).json({ message: "success", data: data });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
