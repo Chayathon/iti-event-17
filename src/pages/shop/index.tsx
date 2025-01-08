c0dc13884b96e79f5703a8b34917659ee91bd29d
Chayathon <gusgus157903@gmail.com>
2025-01-08 12:53:13 +0700

    Update UI

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -55,7 +55,7 @@ export default function ShopPage({}: Props) {
           // title loading
           <div className="flex justify-center mt-4 ">
             <div className="w-16 h-16 border-t-2 border-white rounded-full animate-spin"></div>
-            <p className="text-center text-white py-2">กำลังโหลดข้อมูล...</p>
+            <p className="text-center text-lg text-white p-2">กำลังโหลดข้อมูล...</p>
           </div>
         )}
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
