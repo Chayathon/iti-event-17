cf0a6077c7085c9ebab603522f05013a5a61df2d
Chayathon <gusgus157903@gmail.com>
2025-02-28 15:59:18 +0700

    Update schedule.tsx

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function schedule() {
   return (
-    <img src="https://jpajjgeqekhebztqwhkr.supabase.co/storage/v1/object/public/attachments/image/schedule_iti-event.pdf" alt="schedule" />
+    <img src="https://jpajjgeqekhebztqwhkr.supabase.co/storage/v1/object/public/attachments/image/schedule.jpg" alt="schedule" />
   )
 }
 
